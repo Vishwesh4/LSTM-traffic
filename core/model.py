3103b781f745e66731cff0d16067611e8653d367
Vishwesh Ramanathan <vishweshramanathan@gmail.com>
2019-07-27 20:47:04 +0530

    first commit

---
+++ core/model.py
@@ -0,0 +1,73 @@
+import os
+import math
+import numpy as np
+import datetime as dt
+from numpy import newaxis
+from core.utils import Timer
+from keras.layers import Dense, Activation, Dropout, LSTM
+from keras.models import Sequential, load_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.utils import np_utils
+class Model():
+	"""A class for an building and inferencing an lstm model"""
+
+	def __init__(self):
+		self.model = Sequential()
+
+	def load_model(self, filepath):
+		print('[Model] Loading model from file %s' % filepath)
+		self.model = load_model(filepath)
+
+	def build_model(self, configs):
+		timer = Timer()
+		timer.start()
+
+		for layer in configs['model']['layers']:
+			neurons = layer['neurons'] if 'neurons' in layer else None
+			dropout_rate = layer['rate'] if 'rate' in layer else None
+			activation = layer['activation'] if 'activation' in layer else None
+			return_seq = layer['return_seq'] if 'return_seq' in layer else None
+			input_timesteps = layer['input_timesteps'] if 'input_timesteps' in layer else None
+			input_dim = layer['input_dim'] if 'input_dim' in layer else None
+
+			if layer['type'] == 'dense':
+				self.model.add(Dense(neurons, activation=activation))
+			if layer['type'] == 'lstm':
+				self.model.add(LSTM(neurons, input_shape=(input_timesteps, input_dim), return_sequences=return_seq))
+			if layer['type'] == 'dropout':
+				self.model.add(Dropout(dropout_rate))
+
+		self.model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'],metrics =['mae'])
+		self.model.summary()
+		print('[Model] Model Compiled')
+		timer.stop()
+
+	def train_generator(self, data_gen, epochs, batch_size, steps_per_epoch, save_dir):
+		timer = Timer()
+		timer.start()
+		print('[Model] Training Started')
+		print('[Model] %s epochs, %s batch size, %s batches per epoch' % (epochs, batch_size, steps_per_epoch))
+		
+		save_fname = os.path.join(save_dir, '%s-e%s.h5' % (dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(epochs)))
+		callbacks = [
+			ModelCheckpoint(filepath=str(save_fname), monitor='loss', save_best_only=True)
+		]
+		self.model.fit_generator(
+			data_gen,
+			steps_per_epoch=steps_per_epoch,
+			epochs=epochs,
+			callbacks=callbacks,
+			workers=1,verbose=1
+		)
+		
+		print('[Model] Training Completed. Model saved as %s' % save_fname)
+		timer.stop()
+
+	def predict_point_by_point(self, data):
+		#Predict each timestep given the current and last sequences of true data, in effect only predicting 1 step ahead each time
+		# dic = {0:-1,1:0,2:1}
+		print('[Model] Predicting Point-by-Point...')
+		predicted = self.model.predict(data)
+		# predicted_decoded = [dic[i] for i in np.argmax(predicted,axis=1)]
+		return predicted
+
